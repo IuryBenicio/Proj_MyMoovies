dcda832c97fc7a2ed67975b8f968112ebdb50c0e
IuryBenicio <iury620@gmail.com>
2025-04-05 10:29:02 -0300

    consertei

--- FrontEnd/src/helpers/getApi.ts
+++ FrontEnd/src/helpers/getApi.ts
@@ -1,6 +1,4 @@
 export const urlDefault: string = "https://api.themoviedb.org/3";
 export const apiKeyNumber: string = "19044a28396587997ecdb0f5aa825cea";
 
-export const bancoDeDados: string =
-  "https://projmymoovies-production.up.railway.app";
-// export const bancoDeDados: string = "http://localhost:8000";
+export const bancoDeDados: string = "https://mymovies-yqmh.onrender.com";
