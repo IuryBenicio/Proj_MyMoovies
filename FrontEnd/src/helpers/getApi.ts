f9febd828a98be0a82414cee058f34e2a8d346b7
IuryBenicio <iury620@gmail.com>
2025-04-19 12:30:57 -0300

    Levantando banco de dados na nuvem

--- FrontEnd/src/helpers/getApi.ts
+++ FrontEnd/src/helpers/getApi.ts
@@ -1,9 +1,9 @@
 export const urlDefault: string = "https://api.themoviedb.org/3";
 export const apiKeyNumber: string = "19044a28396587997ecdb0f5aa825cea";
 
-// export const bancoDeDados: string =
-//   "https://projmymoovies-production.up.railway.app";
-export const bancoDeDados: string = "http://localhost:8000";
+export const bancoDeDados: string =
+  "https://projmymoovies-production.up.railway.app";
+// export const bancoDeDados: string = "http://localhost:8000";
 
 // projmymoovies-production.up.railway.app
 // http://localhost:5173/
